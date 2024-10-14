a028beec05549016e58500d327f43fe1c15d06a5
Trevor Piltch <trevor@piltch.com>
2024-10-14 17:57:06 -0400

    Remove old code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# embedded_template
-Template for an embedded rust system on a microbit V2
+# Micro:Bit V2 Cargo Template
+This project is a sample Cargo project for the [Micro:Bi V2](https://microbit.org/) microcontroller. It is written in Rust based on the [Rust Discovery Book](https://github.com/rust-embedded/discovery/). 
 
 ## Usage
-Change the package name in Cargo.toml
-Change directory name
+
+
--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,18 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use rtt_target::{rtt_init_print, rprintln};
-use panic_rtt_target as _;
+use panic_halt as _;
 use microbit::{
     board::Board,
     display::blocking::Display,
-    hal::{prelude::*, Timer},
+    hal::Timer
 };
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();
-    let mut board = Board::take().unwrap();
+    let board = Board::take().unwrap();
 
     let mut timer = Timer::new(board.TIMER0);
-
     let mut display = Display::new(board.display_pins);
 
     let mut light_mtx = [
@@ -27,23 +24,9 @@ fn main() -> ! {
         [0,0,0,0,0],
         [0,0,0,0,0],
     ];
-    let mut index = (0, 0);
-
-    const PIXELS: [(usize, usize); 16] = [
-        (0,0), (0, 1), (0, 2), (0, 3), (0, 4),
-        (1, 4), (2, 4), (3, 4), (4, 4),
-        (4, 3), (4, 2), (4, 1), (4, 0),
-        (3, 0), (2, 0), (1,0)
-    ];
-
-    let mut last_led = (0,0);
 
     loop {
-        for current_led in PIXELS.iter() {
-            light_mtx[last_led.0][last_led.1] = 0;
-            light_mtx[current_led.0][current_led.1] = 1;
-            display.show(&mut timer, light_mtx, 30);
-            last_led = *current_led;
-        }
+        light_mtx[0][0] = (light_mtx[0][0] + 1) % 2;
+        display.show(&mut timer, light_mtx, 1000);
     }
 }
